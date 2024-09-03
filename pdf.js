9c57e4a776a97df9c05e45dd1ebdbe626dd6e88b
Carlos Eduardo Turina <146773453+anirutsolrak@users.noreply.github.com>
2024-09-03 14:34:42 -0300

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -1,70 +1,132 @@
-import jsPDF from 'jspdf';
+import { jsPDF } from 'jspdf';
 import autoTable from 'jspdf-autotable';
-import { extractTableData, extractTableHeaders } from './utils.js'; // Importa as funções
+import { extractTableData, extractTableHeaders } from './utils.js';
 
-// Função para gerar o arquivo PDF
 export function exportToPDF() {
-  const doc = new jsPDF();
-  let startY = 30; // Posição inicial Y para a primeira tabela
+  const { jsPDF } = window.jspdf;
+  const doc = new jsPDF({
+    orientation: 'landscape'
+  });
 
-  // Adiciona o título principal ao PDF
-  const mainTitle = document.querySelector('h2').textContent.trim();
-  const subtitle = document.querySelector('h3').textContent.trim();
-  doc.setFontSize(16);
-  doc.text(mainTitle, 10, 10);
-  doc.setFontSize(14);
-  doc.text(subtitle, 10, 18);
+  const newLineHeightFactor = 0.5;
+  doc.setLineHeightFactor(newLineHeightFactor);
 
-  // Adiciona as tabelas ao PDF usando autoTable
-  addTablesToPDF(doc, startY);
-
-  doc.save('RelatorioCompleto.pdf');
-}
-
-// Função para adicionar todas as tabelas ao PDF
-function addTablesToPDF(doc, startY) {
+  // Seleciona todas as tabelas que você quer incluir no PDF
   const tables = document.querySelectorAll('.table.table-responsive.table-striped.table-bordered.table-sm');
 
-  tables.forEach((table) => {
-    const tableData = extractTableData(table);
-    const tableHeaders = extractTableHeaders(table);
+  let finalY = 30; // Posição inicial Y
 
-    startY += 30;
+  tables.forEach((table, index) => {
+    let titleElement = table.previousElementSibling; // Assume que o título é o elemento anterior à tabela
 
-    autoTable(doc, {
-      head: [tableHeaders],
-      body: tableData,
-      startY: startY,
-      styles: { halign: 'right' },
-      didDrawPage: function (data) {
-        if (data.pageNumber > 1) {
-          doc.setFontSize(10);
-          doc.text("HOSPITAL REGIONAL DA COSTA LESTE MAGID THOMÉ", 10, 10);
-          doc.text("RL06 - CUSTO SERVIÇOS AUXILIARES", 10, 15);
-        }
+    // Verifica se o elemento anterior é um título (h2 ou h3)
+    while (titleElement && (titleElement.tagName !== 'H3' && titleElement.tagName !== 'H2')) {
+      titleElement = titleElement.previousElementSibling;
+    }
+
+    if (titleElement) {
+      let titleHeight = doc.getTextDimensions(titleElement.textContent).h;
+      let titleX = doc.internal.pageSize.width / 2;
+      let titleY = finalY + titleHeight + 5;
+      doc.setFontSize(10);
+      doc.text(titleElement.textContent, titleX, titleY, {
+        align: 'center',
+      });
+      finalY = titleY; // Atualiza finalY para incluir a altura do título
+    }
+
+    doc.autoTable({
+      html: table,
+      startY: finalY + 10,
+      theme: 'grid',
+      headStyles: {
+        fillColor: [200, 230, 255]
       },
+      bodyStyles: {
+        fillColor: false
+      },
+      alternateRowStyles: {
+        fillColor: false
+      },
+      pageBreak: 'auto',
+      rowPageBreak: 'noWrap',
+      overFlow: "linebreak",
+      showHead: 'everyPage',
+      margin: {
+        top: 30,
+        right: 10,
+        bottom: 5,
+        left: 15
+      },
+      styles: {
+        fontSize: 7,
+        fontStyle: 'normal',
+        fontFamily: 'Calibri, sans-serif'
+      },
+      columnStyles: {
+        0: {
+          halign: 'left'
+        },
+        1: {},
+        2: {},
+        3: {},
+        4: {},
+        5: {},
+        6: {},
+        7: {},
+        8: {},
+        9: {},
+        10: {},
+        11: {},
+        12: {},
+        13: {}
+      },
+
       didParseCell: function (data) {
-        if (data.cell.raw && data.cell.raw.includes('<strong>'))  {
-          data.cell.styles.fontStyle = 'bold';
-          data.cell.text = data.cell.raw.replace(/<strong>/g, '').replace(/<\/strong>/g, '');
+        if (data.row.index === 0 && data.section === 'body') {
+          data.cell.styles.fillColor = [200, 230, 255];
+          data.cell.styles.textColor = [0, 0, 0];
+        }
+        if (data.section === 'body' && data.column.index === 0) {
+          data.cell.styles.halign = 'left';
+        }
+        if (data.section === 'body' && data.column.index !== 0) {
+          data.cell.styles.halign = 'center';
         }
       }
     });
 
-    startY = doc.lastAutoTable.finalY;
+    finalY = doc.lastAutoTable.finalY || finalY;
   });
 
-  doc.setFontSize(9);
-  doc.setTextColor(0, 102, 0);
-  doc.text("Sistemas integrados www.cjpnet.com.br", 10, doc.internal.pageSize.getHeight() - 10);
+  addDynamicTextAndPageNumbers(doc);
+  doc.save(document.querySelector('h3').textContent + '.pdf');
+}
 
-  // Adiciona a numeração de páginas ao PDF
+function addDynamicTextAndPageNumbers(doc) {
   const pageCount = doc.internal.getNumberOfPages();
+  const dynamicTexts = [
+    document.querySelector('h2').textContent.trim() + " - Início do Projeto: " + document.querySelectorAll('.col-sm-3 p')[1]?.textContent,
+    document.querySelector('h3').textContent.trim() + " " + document.querySelector('.col-sm-6 p')?.textContent,
+  ];
+
   for (let i = 1; i <= pageCount; i++) {
     doc.setPage(i);
+    let yPos = 10;
+    dynamicTexts.forEach(text => {
+      doc.text(text, 155, yPos, {
+        align: 'center',
+        fontSize: 10
+      });
+      yPos += 7;
+    });
+
     const pageWidth = doc.internal.pageSize.width;
     const pageHeight = doc.internal.pageSize.height;
-    doc.setFontSize(8); 
-    doc.text(`Página ${i} de ${pageCount}`, pageWidth - 10, pageHeight - 5, { align: 'right' });
+    const str = 'Página ' + i + ' de ' + pageCount;
+    doc.setFontSize(6);
+    doc.text(str, pageWidth - 10, pageHeight - 3, {
+      align: 'right'
+    });
   }
 }
