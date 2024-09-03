5d1192c5f554c2629b8540d18f1f9397375a4860
Carlos Eduardo Turina <146773453+anirutsolrak@users.noreply.github.com>
2024-09-03 14:17:56 -0300

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -7,14 +7,15 @@ export function exportToPDF() {
   const doc = new jsPDF();
   let startY = 30; // Posição inicial Y para a primeira tabela
 
-  // Adiciona o título principal ao PDF em uma única linha
+  // Adiciona o título principal ao PDF
   const mainTitle = document.querySelector('h2').textContent.trim();
   const subtitle = document.querySelector('h3').textContent.trim();
   doc.setFontSize(16);
-  doc.text(mainTitle, 10, 10, { noWrap: true }); // Adicionado noWrap: true
+  doc.text(mainTitle, 10, 10);
   doc.setFontSize(14);
-  doc.text(subtitle, 10, 18, { noWrap: true }); // Adicionado noWrap: true
+  doc.text(subtitle, 10, 18);
 
+  // Adiciona as tabelas ao PDF usando autoTable
   addTablesToPDF(doc, startY);
 
   doc.save('RelatorioCompleto.pdf');
@@ -28,7 +29,7 @@ function addTablesToPDF(doc, startY) {
     const tableData = extractTableData(table);
     const tableHeaders = extractTableHeaders(table);
 
-    startY += 10; // Ajuste o espaçamento entre as tabelas
+    startY += 30;
 
     autoTable(doc, {
       head: [tableHeaders],
@@ -38,12 +39,12 @@ function addTablesToPDF(doc, startY) {
       didDrawPage: function (data) {
         if (data.pageNumber > 1) {
           doc.setFontSize(10);
-          doc.text("HOSPITAL REGIONAL DA COSTA LESTE MAGID THOMÉ", 10, startY - 20); // Título na segunda página
-          doc.text("RL06 - CUSTO SERVIÇOS AUXILIARES", 10, startY - 10); // Subtítulo na segunda página
+          doc.text("HOSPITAL REGIONAL DA COSTA LESTE MAGID THOMÉ", 10, 10);
+          doc.text("RL06 - CUSTO SERVIÇOS AUXILIARES", 10, 15);
         }
       },
       didParseCell: function (data) {
-        if (data.cell.raw && data.cell.raw.includes('<strong>')) {
+        if (data.cell.raw && data.cell.raw.includes('<strong>'))  {
           data.cell.styles.fontStyle = 'bold';
           data.cell.text = data.cell.raw.replace(/<strong>/g, '').replace(/<\/strong>/g, '');
         }
@@ -57,12 +58,13 @@ function addTablesToPDF(doc, startY) {
   doc.setTextColor(0, 102, 0);
   doc.text("Sistemas integrados www.cjpnet.com.br", 10, doc.internal.pageSize.getHeight() - 10);
 
+  // Adiciona a numeração de páginas ao PDF
   const pageCount = doc.internal.getNumberOfPages();
   for (let i = 1; i <= pageCount; i++) {
     doc.setPage(i);
     const pageWidth = doc.internal.pageSize.width;
     const pageHeight = doc.internal.pageSize.height;
-    doc.setFontSize(8);
+    doc.setFontSize(8); 
     doc.text(`Página ${i} de ${pageCount}`, pageWidth - 10, pageHeight - 5, { align: 'right' });
   }
 }
